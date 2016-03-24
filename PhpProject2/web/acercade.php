94491b9f615bd2ce22804cc7b31ea34ace88bdf3
unknown <cesar.b.c@hotmail.com>
2016-03-24 01:23:04 -0600

    no message

---
+++ PhpProject2/web/acercade.php
@@ -0,0 +1,98 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include './recursosreusables/head.php';
+        ?>
+    </head>
+    <body>
+        <!-- header -->
+        <?php
+        include './recursosreusables/header.php';
+        ?>
+        <!-- //header -->
+
+        <!-- about -->
+        <div class="about">
+            <div class="about-heading">
+                <div class="container">
+                    <h2 class="wow fadeInDown animated" data-wow-delay=".5s">¿Quienes somos?</h2>
+                    <p class="wow fadeInUp animated" data-wow-delay=".5s">  Smart Tour CR es una compañia dedicada a sugerir paquetes turisticos dentro del territorio costarricense
+                        basandonos en criterios como los intereses del usuario, tiempo que se tardara realizando el tour,
+                        entre otros</p>
+                </div>
+            </div>
+            <div class="about-top">
+                <div class="container">
+                    <div class="about-bottom">
+                        <img class="wow fadeInUp animated" data-wow-delay=".5s" src="images/ab.jpg" alt="" />
+                        <p class="wow fadeInUp animated" data-wow-delay=".5s">  Smart Tour CR es una compañia dedicada a sugerir paquetes turisticos dentro del territorio costarricense
+                            basandonos en criterios como los intereses del usuario, tiempo que se tardara realizando el tour,
+                            entre otros</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- team -->
+            <div class="team">
+                <div class="container">
+                    <div class="popular-heading team-heading">
+                        <h3 class="wow fadeInUp animated" data-wow-delay=".5s">Nuestro equipo</h3>
+                        <p class="wow fadeInUp animated" data-wow-delay=".5s">Somos un grupo de estudiantes de cuarto año de informatica empresarial 
+                                                                                en el recinto de Paraiso de la Universidad de Costa Rica</p>
+                    </div>
+                    <div class="team-grids">
+                        <div class="col-md-4 team-grid wow fadeInLeft animated" data-wow-delay=".5s">
+                            <img src="images/t1.jpg" alt="" />
+                            <h4>Cesar Obando</h4>
+                            <div class="social-icons team-icons">
+                                <ul>
+                                    <li><a class="twitter" href="https://twitter.com/CesarObandoS"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a class="twitter facebook" href="https://www.facebook.com/cesar.obando.5"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a class="twitter google" href="#"><i class="fa fa-google-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-4 team-grid wow fadeInUp animated" data-wow-delay=".5s">
+                            <img src="images/t2.jpg" alt="" />
+                            <h4>Jonathan Bonilla</h4>
+                            <div class="social-icons team-icons">
+                                <ul>
+                                    <li><a class="twitter" href="https://twitter.com/jonabu94"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a class="twitter facebook" href="https://www.facebook.com/jonathan.bonillaumana"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a class="twitter google" href="#"><i class="fa fa-google-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-4 team-grid wow fadeInRight animated" data-wow-delay=".5s">
+                            <img src="images/t3.jpg" alt="" />
+                            <h4>Allan Moya</h4>
+                            <div class="social-icons team-icons">
+                                <ul>
+                                    <li><a class="twitter" href=""><i class="fa fa-twitter"></i></a></li>
+                                    <li><a class="twitter facebook" href="https://www.facebook.com/allan.moyamadrigal"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a class="twitter google" href="#"><i class="fa fa-google-plus"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="clearfix"> </div>
+                    </div>
+                </div>
+            </div>
+            <!-- //team -->
+        </div>
+        <!-- //about -->
+
+        <!-- footer -->
+        <?php
+            include './recursosreusables/footer.php';
+        ?>
+        <!-- //footer -->
+    </body>	
+</html>
