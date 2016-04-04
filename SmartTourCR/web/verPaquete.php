d49610db8fd8795f59a3bf3c019178a2890c5a37
allanMoyaM <allanmm29@gmail.com>
2016-04-04 11:55:14 -0600

    verPaquete

--- SmartTourCR/web/verPaquete.php
+++ SmartTourCR/web/verPaquete.php
@@ -1,10 +1,15 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
 <!DOCTYPE html>
 <html>
     <head>
         <?php
         include './recursosreusables/head.php';
         ?>
-        <link href="estilosVer.css" rel="stylesheet"/>
     </head>
     <body>
         <!-- header -->
@@ -13,66 +18,34 @@
         ?>
         <!-- //header -->
 
-        
-
-
-        <section>
-            <!-- blog -->
-            <div class="blog">
-                <!-- container -->
-                <div class="container">
-                    <h3>Ver Paquete</h3>
-                    <div class="col-md-8 blog-top-left-grid">
-                        <div class="left-blog left-single">
-                            <div class="blog-left">
-                                <div class="single-left-left wow fadeInUp animated" data-wow-delay=".5s">
-                                    <h3>Volcán Irazú</h3>
-                                    <img src="images/volcanIrazu2.jpg" alt="" />
-                                </div>
-                                <div class="blog-left-bottom wow fadeInUp animated" data-wow-delay=".5s">
-                                    <h3>Descripción</h3>
-                                    <p>Parque Nacional Volcán Irazú, se puede disfrutar de una buena vista hacia el
-                                       valle central, además de una flora y fauna muy variada. 
-                                    </p>
-                                </div>
-                                
-                            </div>
-                            <div class="response wow fadeInUp animated" data-wow-delay=".5s">
-                                
-                                
-                            </div>
-
-                            
-                        </div>
+        <!-- login -->
+        <div class="login">
+            <div class="container">
+                <div class="login-body">
+                    <div class="login-heading">
+                        <h1>Ver Paquete</h1>
                     </div>
-                    <div class="col-md-4 blog-top-right-grid">
-                        <div class="Categories wow fadeInUp animated" data-wow-delay=".5s">
-                            <h3>Información del paquete</h3>
-                            <ul>
-                                <p>Lugar:Volcan Irazu, Cartago</p>
-                                <p>Clima:Frio</p>
-                                <p>Precio:1000</p>
-                                <p>Tipo de calle:Asfaltada</p>
-                                <p>Tiempo:1:30</p>
-                                <input type="submit" name="marcarFavorito" value="Favorito" class="btn-primary btn-lg">
-                                
-                                
-                            </ul>
-                        </div>
-                        
+                    <div class="login-info">
+                        <form action="editarusuario.php">
+                            <label type="text" name="lugar">Lugar:  Volcán Irazú</label><br><br>
+                            <label type="text" name="clima">Clima:  Frío</label><br><br>
+                            <label type="text" name="tipoCalle">Tipo de calle:  Asfaltada, en buen estado</label><br><br>
+                            <label type="text" name="precio">Precio:  1000</label><br><br>
+                            <label type="text" name="tiempo">Tiempo:  2:30 hrs</label><br><br>
+                            <figure> <img src="images/volcanIrazu2.jpg" alt="VolcanIrazu" width="200" height="200"> <figcaption>Descripción: Parque Nacional Volcán Irazú, gran variedad en flora y fauna, con una excelente vista hacia todo el valle central.</figcaption></figure>
+                            <input type="submit" name="favorito" value="Favorito">
+                            <input type="submit" name="atras" value="Atrás">
+                        </form>
                     </div>
-                    <div class="clearfix"> </div>
                 </div>
-                <!-- //container -->
             </div>
-            <!-- //blog -->
-        </section>
-        <footer>
-            <?php
-            include './recursosreusables/footer.php';
-            ?>
-        </footer>
-    </body>
+        </div>
+        <!-- //login -->
 
-
+        <!-- footer -->
+        <?php
+        include './recursosreusables/footer.php';
+        ?>
+        <!-- //footer -->
+    </body>	
 </html>
